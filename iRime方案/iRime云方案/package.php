818ba1fbaf0fc7303d69f908ef32985c9f2478a8
jimmy54 <jimmy54@126.com>
2020-06-13 23:41:54 +0800

    feat:打包脚本

---
+++ iRime方案/iRime云方案/package.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function directory_map($source_dir, $directory_depth = 0, $hidden = FALSE)
+    {
+        if ($fp = @opendir($source_dir))
+        {
+            $filedata  = array();
+            $new_depth	= $directory_depth - 1;
+            $source_dir	= rtrim($source_dir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+            while (FALSE !== ($file = readdir($fp)))
+            {
+                // Remove '.', '..', and hidden files [optional]
+                if ($file === '.' OR $file === '..' OR ($hidden === FALSE && $file[0] === '.'))
+                {
+                    continue;
+                }
+                is_dir($source_dir.$file) && $file .= DIRECTORY_SEPARATOR;
+                if (($directory_depth < 1 OR $new_depth > 0) && is_dir($source_dir.$file))
+                {
+                    $filedata[$file] = directory_map($source_dir.$file, $new_depth, $hidden);
+                }
+                else
+                {
+                    $filedata[] = $file;
+                }
+            }
+            closedir($fp);
+            return $filedata;
+        }
+        return FALSE;
+    }
+echo "path:".print_r(directory_map(__DIR__, 2), true);
+?>
