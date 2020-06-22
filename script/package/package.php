cae15e38720e8859a4296b4893a74a4669153ea9
jimmy54 <jimmy54@126.com>
2020-06-22 21:04:01 +0800

    chore:add log

--- script/package/package.php
+++ script/package/package.php
@@ -31,6 +31,7 @@ class Package
         $this->accessKeySecret = getenv("ACCESSKEYSECRET");
         $this->endpoint = getenv("ENDPOINT");
         $this->bucket = getenv("BUCKET");
+        echo "buck:"$this->bucket."\n";
         $this->uploadSchemaUrl = getenv("UPLOADSCHEMAURL");
     }
 
@@ -103,6 +104,7 @@ class Package
                 $schemaName = substr($key, 0, $pos);
                 try {
                     $schemaDetail = Yaml::parseFile(self::SCHEMA_PATH . "/" . $key . "/" . $schemaName . ".schema.yaml");
+                    echo "read schema :".$key." detail:".print_r($schemaDetail['schema'], true)."\n";
                     $schemaHead = $schemaDetail['schema'];
                     $lasUpdateSchema[$key] = $schemaHead;
                 } catch (ParseException $e) {
